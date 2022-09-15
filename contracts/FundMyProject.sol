3b1de469071f2b4e791466df648a554bbdb2ba93
Carlos Calderon <cdcalderon@gmail.com>
2022-09-15 08:07:45 -0500

    Implement fund method, refactor to use library

--- contracts/FundMyProject.sol
+++ contracts/FundMyProject.sol
@@ -1,23 +1,22 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+import "./PriceConverter.sol";
 
 contract FundMe {
-    uint256 public minimumUsd = 50;
+    using PriceConverter for uint256;
+
     uint256 public constant MINIMUM_USD = 50 * 10**18;
+    address[] public funders;
+    mapping(address => uint256) addressToAmountFounded;
 
     function fund() public payable {
         require(
             msg.value.getConversionRate() >= MINIMUM_USD,
             "You need to spend more ETH!"
         );
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
-        );
-        return priceFeed.version();
+        funders.push(msg.sender);
+        addressToAmountFounded[msg.sender] = msg.value;
     }
 
     fallback() external payable {
